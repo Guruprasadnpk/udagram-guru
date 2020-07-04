9004b7a5b30c3c47318388cc598036a023198955
Guru Kuppuswamy <guruprasad.npk@gmail.com>
2020-07-04 16:21:22 -0700

    Test Build

--- deployment/updateKube.sh
+++ deployment/updateKube.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-feed-deployment.yml
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-user-deployment.yml
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/frontend-deployment.yml
+pwd
+#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-feed-deployment.yml
+#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-user-deployment.yml
+#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/frontend-deployment.yml
 sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/reverseproxy-deployment.yml
