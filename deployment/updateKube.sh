4acec2173eb9b5f9b457feff985e4e597bb0349a
Guru Kuppuswamy <guruprasad.npk@gmail.com>
2020-07-04 16:39:34 -0700

    Fix build

--- deployment/updateKube.sh
+++ deployment/updateKube.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pwd
-#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-feed-deployment.yml
-#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-user-deployment.yml
-#sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/frontend-deployment.yml
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/reverseproxy-deployment.yml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-feed-deployment.yaml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/backend-user-deployment.yaml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/frontend-deployment.yaml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" deployment/k8s/reverseproxy-deployment.yaml
