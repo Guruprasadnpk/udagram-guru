12af70f055c374573e9389ea8f10c3d406e7d1e3
Guru Kuppuswamy <guruprasad.npk@gmail.com>
2020-07-04 20:04:37 -0700

    Enable deployments

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,26 +7,26 @@ echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
   --token=$KUBERNETES_TOKEN \
   apply -f ./deployment/k8s
 echo "The build number is ${TRAVIS_BUILD_NUMBER}"
-#/usr/local/bin/kubectl \
-#  --kubeconfig=/dev/null \
-#  --server=$KUBERNETES_SERVER \
-#  --certificate-authority=cert.crt \
-#  --token=$KUBERNETES_TOKEN \
-#  set image deployment/backend-feed backend-feed=${DOCKER_USERNAME}/udacity-backend-feed:${TRAVIS_BUILD_NUMBER} --record
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/backend-feed backend-feed=${DOCKER_USERNAME}/udacity-backend-feed:${TRAVIS_BUILD_NUMBER} --record
 
-#/usr/local/bin/kubectl \
-#  --kubeconfig=/dev/null \
-#  --server=$KUBERNETES_SERVER \
-#  --certificate-authority=cert.crt \
-#  --token=$KUBERNETES_TOKEN \
-#  set image deployment/backend-user backend-user=${DOCKER_USERNAME}/udacity-backend-user:${TRAVIS_BUILD_NUMBER} --record
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/backend-user backend-user=${DOCKER_USERNAME}/udacity-backend-user:${TRAVIS_BUILD_NUMBER} --record
 
-#/usr/local/bin/kubectl \
-#  --kubeconfig=/dev/null \
-#  --server=$KUBERNETES_SERVER \
-#  --certificate-authority=cert.crt \
-#  --token=$KUBERNETES_TOKEN \
-#  set image deployment/frontend frontend=${DOCKER_USERNAME}/udacity-frontend:${TRAVIS_BUILD_NUMBER} --record
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  set image deployment/frontend frontend=${DOCKER_USERNAME}/udacity-frontend:${TRAVIS_BUILD_NUMBER} --record
 
 /usr/local/bin/kubectl \
   --kubeconfig=/dev/null \
